b1e411f9f40694065ea910c9fd5121a63108ad11
THRYLLR <55361185+THRYLLR@users.noreply.github.com>
2020-06-22 19:52:39 -0700

    stuff

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,16 @@ const browserSync = require("browser-sync").create();
 gulp.task("default", async () => {
   return console.log("Success!");
 });
+
+gulp.task("serve:dev", async () => {
+  browserSync.init({
+    server: "build",
+    port: 3005,
+    ghostMode: false,
+    logPrefix: "Untitled-Base-Building",
+    online: false,
+    reloadOnRestart: true,
+    reloadDebounce: 1000,
+    notify: false,
+  });
+});
